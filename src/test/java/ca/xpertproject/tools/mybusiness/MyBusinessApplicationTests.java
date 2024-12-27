ee6cd94453319cecac9390f7d592715b8f4d464d
Fabrice POULIN <fabrice.poulin7@gmail.com>
2024-12-27 14:45:06 -0500

    First commit

---
+++ src/test/java/ca/xpertproject/tools/mybusiness/MyBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.xpertproject.tools.mybusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
